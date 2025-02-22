7a64005315ed40e2ab00381ae9c6fc395d4668fa
like-cent <pasindulakshan3599@gmail.com>
2025-02-22 08:25:47 +0530

    Update session.js

--- session.js
+++ session.js
@@ -40,6 +40,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Your session id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~CYUkEIrS#oRBJve5eH6CN48Rm3nOQRCMC6Cmuswx0ZFoyf8OxOkg",
 PORT: process.env.PORT || "8000"
 };
